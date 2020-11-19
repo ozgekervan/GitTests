be3fcbc68ce5853fe460fa2146dc083befa989cd
Zafer CÖMERT <zcomert@samsun.edu.tr>
2020-11-19 21:14:38 +0300

    Yazdir()

---
+++ GitTests/Matris.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitTests
+{
+    public class Matris
+    {
+        private int[,] M { get; set; }
+        public Matris(int n=3)
+        {
+            M = new int[n, n];
+        }
+
+        public void Yazdir()
+        {
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for (int j = 0; j < M.GetLength(0); j++)
+                {
+                    Console.Write("{0,5}",M[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
